a4cfcf928308d880db8e9690d725d108ccb9bcb7
dannis <dannistangkw@gmail.com>
2022-06-08 22:01:24 +0800

    spacing

--- DWM.py
+++ DWM.py
@@ -10,7 +10,6 @@ import datetime
 import urllib.parse
 import pandas as pd
 
-
 #########Keys & URL########################################
 Binance_API_KEY = config.Binance_keyPrivate
 Binance_SECRET_KEY = config.Binance_keySecret
@@ -26,8 +25,6 @@ Col = ['Platform', 'Currency', 'Tenor', 'APR', 'Min_Subscribe', 'Max_Subscribe']
 # #Datetime -> HK_time
 datetime = datetime.datetime.fromtimestamp(int(timestamp) / 1000)  # using the local timezone
 Today_date = datetime.strftime("%Y-%m-%d")
-
-
 
 ###headers##############################################
 Binance_headers = {
@@ -51,6 +48,7 @@ class BinanceException(Exception):
 def get_api_data():
     timestamp = int(time.time() * 1000)
     Result_list = []
+    len_reslut_list = 0
     #####params
     Binance_params = {
         'timestamp': timestamp,
@@ -58,9 +56,9 @@ def get_api_data():
         'size': 100
     }
 
-
     query_string = urlencode(Binance_params)
-    Binance_params['signature'] = hmac.new(Binance_SECRET_KEY.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
+    Binance_params['signature'] = hmac.new(Binance_SECRET_KEY.encode('utf-8'), query_string.encode('utf-8'),
+                                           hashlib.sha256).hexdigest()
 
     Binance_url = urljoin(Binance_BASE_URL, Binance_PATH)
     Binance_r = requests.get(Binance_url, headers=Binance_headers, params=Binance_params)
@@ -74,14 +72,16 @@ def get_api_data():
                 Binance_result.append(Binance_data['duration'])
                 Binance_result.append("{:.2%}".format(float(Binance_data['interestRate'])))
                 Binance_result.append(int(Binance_data['lotSize']))
-                Binance_result.append(f"{int(Binance_data['lotsUpLimit'])*int(Binance_data['lotSize'])/1000000}M")
+                Binance_result.append(f"{int(Binance_data['lotsUpLimit']) * int(Binance_data['lotSize']) / 1000000}M")
+                len_reslut_list = len(Binance_result)
                 Result_list.append(Binance_result)
     else:
         raise BinanceException(status_code=Binance_r.status_code, data=Binance_r.json())
 
+    space = [''] * len_reslut_list
+    Result_list.append(space)
 
     #############WhaleFin###########################################################################################################################################################################################################
-
 
     method = 'GET'
     timestamp = int(time.time() * 1000)
@@ -100,7 +100,6 @@ def get_api_data():
 
     signature = hmac.new(bytes(WhaleFin_SECRET_KEY, 'utf-8'), bytes(signStr, 'utf-8'), hashlib.sha256).hexdigest()
 
-
     headers = {
         'access-key': WhaleFin_API_Key,
         'access-timestamp': str(timestamp),
@@ -118,8 +117,9 @@ def get_api_data():
             Whale_result.append("{:.2%}".format(float(Whale_data['originalApr'])))
             Whale_result.append(int(float(Whale_data['minSubscribeAmount'])))
             if Whale_data['maxIndividualSubscribeAmount'] is not None:
-                Whale_result.append(f"{int(Whale_data['maxIndividualSubscribeAmount'])/1000000}M")
-            else: Whale_data['maxIndividualSubscribeAmount']
+                Whale_result.append(f"{int(Whale_data['maxIndividualSubscribeAmount']) / 1000000}M")
+            else:
+                Whale_result.append('NA')
 
             Result_list.append(Whale_result)
     else:
@@ -128,15 +128,13 @@ def get_api_data():
     return Result_list
 
 
-
 ###########run#####################################################
 def run():
     ##########dataframe##############################################
     Result_list = get_api_data()
     df = pd.DataFrame(columns=Col, data=Result_list)
     file_name = f'Stablecoin_Yield_Overview_{Today_date}.csv'
-    df.to_csv(f'files/{file_name}')
-
+    df.to_csv(f'files/{file_name}', index=False)
 
     ###########telegram
     token = config.telegram_token
